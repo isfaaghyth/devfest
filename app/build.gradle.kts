2d1dd7164e9f4ef5e0b815b7fb5d4dd69571deff
isfaaghyth <isfaaghyth@gmail.com>
2023-11-21 05:03:45 +0700

    fix: compilation error due invalid jvm version target

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,4 +49,8 @@ android {
     defaultConfig {
         minSdk = 24
     }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
 }
