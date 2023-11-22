42a49d365fdaf48c2903d60f8f2d46353367565e
isfaaghyth <isfaaghyth@gmail.com>
2023-11-23 03:35:30 +0700

    feat: create a welcoming wording card

---
+++ app/src/commonMain/kotlin/app/isfa/devfest/components/WelcomeWordingCard.kt
@@ -0,0 +1,32 @@
+package app.isfa.devfest.components
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun WelcomeWordingCard(current: String) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+    ) {
+        Text(
+            text = "Selamat $current 👋🏼",
+            style = MaterialTheme.typography.bodyLarge
+        )
+
+        Text(
+            text = "Welcome to DevFest Indonesia!",
+            style = MaterialTheme.typography.titleLarge,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier
+                .padding(top = 2.dp)
+        )
+    }
+}
